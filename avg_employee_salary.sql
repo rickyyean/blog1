7b487fdd4f8836c535d6fc3dfdffc026546141d9
Chuck Lam <chuck.lam@stanfordalumni.org>
2019-01-21 17:22:28 -0800

    Update avg_employee_salary.sql

--- avg_employee_salary.sql
+++ avg_employee_salary.sql
@@ -1,9 +1,8 @@
 SELECT
-    e.department_id,
     department_name,
     AVG(salary)
 FROM
     employees e
 INNER JOIN departments d ON d.department_id = e.department_id
 GROUP BY
-    e.department_id;
+    department_name;
